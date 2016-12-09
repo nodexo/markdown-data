7d1a06a1a2603c39c2498daa3b3407414d10bf7a
Michael Kortstiege <mk@nodexo.de>
2016-12-09 20:50:28 +0100

    refactor and extend code

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
 
 'use strict'
-// const yaml = require('js-yaml')
-// const moml = require('moml')
+
+const PARSERS = {
+  yaml: require('js-yaml').safeLoad,
+  moml: require('moml').parse,
+  toml: require('toml').parse,
+  json: JSON.parse
+}
+
+const defaultOptions = {
+  silent: false
+}
 
 /**
  * Markdown data static class.
@@ -10,33 +19,86 @@
 class MarkdownData {
 
   /**
-   * Parses markdown string
+   * Merge user options with default options
+   * @param {Object} userOptions
+   * @return {Object}
+   */
+  static _mergeOptions (userOptions) {
+    if (Object.prototype.toString.call(userOptions) === '[object Object]') {
+      for (let key of Object.keys(userOptions)) {
+        if (defaultOptions.hasOwnProperty(key) === false) {
+          throw new Error('Unsupported option: ' + key)
+        }
+      }
+      return Object.assign({}, defaultOptions, userOptions)
+    }
+    return defaultOptions
+  }
+
+  /**
+   * Parse data string
    * @param {String} s
    * @return {Object}
    */
-  static parse (s) {
+  static _parseData (t, s, o) {
+    try {
+      return PARSERS[t](s)
+    } catch (e) {
+      if (o.silent === false) {
+        throw new Error(e.message)
+      }
+    }
+  }
+
+  /**
+   * Parse markdown string
+   * @param {String} s
+   * @return {Object}
+   */
+  static parse (s, userOptions) {
+    const options = this._mergeOptions(userOptions)
     if (!s || typeof s !== 'string') {
+      if (options.silent === false) {
+        throw new Error('The first parameter has to be of type string!')
+      }
       return null
     }
 
     // Extract data blocks
     let datablocks = []
     try {
-      s = s.replace(/^<!--([a-z][a-z0-9:\.]+)\n((.*\n)*?)-->(\n|$)/gm,
-        function (match, $1, $2, offset, original) {
-          datablocks.push({type: $1, data: $2.trim()})
+      s = s.replace(/^<!--([a-z0-9]+):?([a-z0-9-_.]+)?\n((.*\n)*?)-->(\n|$)/gm,
+        function (match, $1, $2, $3, offset, original) {
+          datablocks.push({type: $1, key: $2 || null, data: $3.trim()})
           return ''
         })
     } catch (e) {
       console.log(e.message)
     }
 
+    // Parse blocks
     let data = {}
     for (let db of datablocks) {
-      console.log(db.type)
+      if (PARSERS[db.type]) {
+        try {
+          let obj = {}
+          if (db.key) {
+            obj[db.key] = this._parseData(db.type, db.data, options)
+          } else {
+            obj = this._parseData(db.type, db.data, options)
+          }
+          data = Object.assign({}, data, obj)
+        } catch (e) {
+          console.log(e.message)
+        }
+      } else {
+        if (options.silent === false) {
+          throw new Error(`Unsupported parser: ${db.type}`)
+        }
+      }
     }
 
-    return {markdown: s, extracted_data: data}
+    return {markdown: s, data: data}
   }
 
 }
